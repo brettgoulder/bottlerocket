654863f7b097ecba9b2698ceb9e89f8d02d461c5
Carl Woodward <cjwoodward@gmail.com>
2011-03-28 23:01:09 +1100

    Got split environments working

--- lib/bottlerocket/models/configuration.rb
+++ lib/bottlerocket/models/configuration.rb
@@ -1,4 +1,25 @@
 require 'singleton'
+
+class ConnectionHelper
+  attr_accessor :attrs
+  
+  def initialize(attrs)
+    self.attrs = attrs
+  end
+  
+  def method_missing(field, *args)
+    begin
+      attrs[field.to_s]
+    rescue
+      super(field, *args)
+    end
+  end
+  
+  def connection
+    # TODO Brett to return a mongo connection.
+  end
+end
+
 class Configuration
   include Singleton
   
@@ -8,20 +29,20 @@ class Configuration
     @attrs ||= YAML.load_file config_file_location
   end
   
+  def environments
+    %w(development test staging production)
+  end
+  
   def content_types
     @content_types ||= ContentTypes.load(content_types_file)
   end
   
-  def find(db_type, name)
-    attrs[db_type][name]
-  end
-  
   def method_missing(field, *args)
     begin
-      if field.to_s == 'development' || field.to_s == 'production'
-        @db_type = field.to_s
+      if environments.include? field.to_s
+        ConnectionHelper.new attrs[field.to_s]
       else
-        find(@db_type, field.to_s)
+        attrs[field.to_s]
       end
     rescue
       super(field, *args)
--- spec/bottlerocket/models/configuration_spec.rb
+++ spec/bottlerocket/models/configuration_spec.rb
@@ -8,13 +8,13 @@ describe Configuration do
   it "should have a database name" do
     Configuration.instance.development.database_name.should == "test"
   end
-  # 
-  # it "should return a set of content types" do
-  #   Configuration.instance.content_types.should_not be_empty
-  # end
-  # 
-  # it "should have a hash of users" do
-  #   Configuration.instance.users['admin'].should == "admin"
-  #   Configuration.instance.users['client'].should == "client"
-  # end
+  
+  it "should return a set of content types" do
+    Configuration.instance.content_types.should_not be_empty
+  end
+  
+  it "should have a hash of users" do
+    Configuration.instance.users['admin'].should == "admin"
+    Configuration.instance.users['client'].should == "client"
+  end
 end
