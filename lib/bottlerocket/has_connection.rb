ddc781f56f4ba69a6f75b583ab6e2daa73287965
Brett Goulder <brett.goulder@gmail.com>
2011-03-28 16:10:13 -0600

    Fixing up some tests

--- lib/bottlerocket/has_connection.rb
+++ lib/bottlerocket/has_connection.rb
@@ -1,6 +1,6 @@
 module HasConnection
   def db
-    @db = Mongo::Connection.new(Configuration.instance.database_host,
-                                Configuration.instance.database_port).db(Configuration.instance.database_name)
+    @db = Mongo::Connection.new(Configuration.instance.development.database_host,
+                                Configuration.instance.development.database_port).db(Configuration.instance.development.database_name)
   end
 end
